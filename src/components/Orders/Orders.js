8a221d19009b4ff0d8aea2753ea8b790b6e933c7
iamrealfarhanbd <iamfarhan09@gmail.com>
2022-05-26 23:04:57 +0600

    fixed the orders page

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -76,7 +76,6 @@ const Order = () => {
                 }
             });
     }
-
     return (
         <div className="hero min-h-screen bg-base-200">
             <div className="hero-content flex-col-reverse lg:flex-row-reverse lg:items-start">
